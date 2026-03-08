6fa55bb66e81a7b6f179e7a2323e3f8e2689c736
Xxtorres <jsetorres1997@gmail.com>
2026-03-08 14:58:01 -0300

    fix: disable middleware for demo

--- middleware.ts
+++ middleware.ts
@@ -1,30 +1,31 @@
-import { createServerClient } from "@supabase/ssr";
-import { NextResponse, type NextRequest } from "next/server";
+// import { createServerClient } from "@supabase/ssr";
+// import { NextResponse, type NextRequest } from "next/server";
 
-export async function middleware(request: NextRequest) {
-  let response = NextResponse.next({ request });
+// export async function middleware(request: NextRequest) {
+//   let response = NextResponse.next({ request });
 
-  const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        getAll: () => request.cookies.getAll(),
-        setAll: (cs) =>
-          cs.forEach(({ name, value, options }) => {
-            request.cookies.set(name, value);
-            response.cookies.set(name, value, options);
-          }),
-      },
-    }
-  );
+//   const supabase = createServerClient(
+//     process.env.NEXT_PUBLIC_SUPABASE_URL!,
+//     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+//     {
+//       cookies: {
+//         getAll: () => request.cookies.getAll(),
+//         setAll: (cs) =>
+//           cs.forEach(({ name, value, options }) => {
+//             request.cookies.set(name, value);
+//             response.cookies.set(name, value, options);
+//           }),
+//       },
+//     }
+//   );
 
-  // Refresca el token si expiró
-  await supabase.auth.getUser();
+//   // Refresca el token si expiró
+//   await supabase.auth.getUser();
 
-  return response;
-}
+//   return response;
+// }
 
-export const config = {
-  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
-};
+// export const config = {
+//   matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
+// };
+export {};
